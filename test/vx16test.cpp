c10969a8b87d5786ae98e46c6b07f89382c1cd5a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-08-15 11:38:58 +0300

    + stub test to verify compilation

---
+++ test/vx16test.cpp
@@ -0,0 +1,25 @@
+/*
+ * vx16: Source Code Level Virtual x86 16-bit CPU
+ * Copyright (C) 2016  Alexey Lysiuk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "vx16.h"
+
+int main()
+{
+    vx16::Memory mem;
+    vx16::CPU cpu(mem);
+}
