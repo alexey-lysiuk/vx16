b8e7a40ef2300bdf517c21f614470f83570dae7e
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-08-15 13:10:37 +0300

    + getters for CPU and memory states

--- include/vx16.h
+++ include/vx16.h
@@ -39,7 +39,12 @@ namespace vx16
         uint16_t allocPage()
         {
             m_storage.emplace_back();
-            return static_cast<uint16_t>(m_storage.size() - 1);
+            return pageCount() - 1;
+        }
+
+        uint16_t pageCount() const
+        {
+            return static_cast<uint16_t>(m_storage.size());
         }
 
     private:
@@ -73,6 +78,33 @@ namespace vx16
 
         CPU& operator=(const CPU&) = delete;
         CPU& operator=(CPU&&) = delete;
+
+        uint8_t al() const { return m_al; }
+        uint8_t ah() const { return m_ah; }
+        uint8_t bl() const { return m_bl; }
+        uint8_t bh() const { return m_bh; }
+        uint8_t cl() const { return m_cl; }
+        uint8_t ch() const { return m_ch; }
+        uint8_t dl() const { return m_dl; }
+        uint8_t dh() const { return m_dh; }
+
+        uint16_t ax() const { return m_ax; }
+        uint16_t bx() const { return m_bx; }
+        uint16_t cx() const { return m_cx; }
+        uint16_t dx() const { return m_dx; }
+
+        uint16_t bp() const { return m_bp; }
+        uint16_t si() const { return m_si; }
+        uint16_t di() const { return m_di; }
+        uint16_t sp() const { return m_sp; }
+
+        uint16_t ds() const { return m_ds; }
+        uint16_t ss() const { return m_ss; }
+        uint16_t es() const { return m_es; }
+        uint16_t fs() const { return m_fs; }
+        uint16_t gs() const { return m_gs; }
+
+        uint16_t flags() const { return m_flags; }
 
     private:
         Memory& m_memory;
